4ad21c5fcfc2e93c95f18d8c56e3788eb3d1ca61
Adodoadjie <eadodoadjie@yahoo.com>
2024-01-16 13:01:43 +0000

    first commit

---
+++ dictionary/src/data/data.js
@@ -0,0 +1,42 @@
+const deFaultdata = {
+  word: "keyboard",
+  meanings: [
+    {
+      definitions: [
+        {
+          definition:
+            "(etc.) A set of keys used to operate a typewriter, computer etc.",
+        },
+        {
+          definition:
+            "A component of many instruments including the pian…cause different tones to be produced when struck.",
+        },
+        {
+          definition:
+            "A device with keys of a musical keyboard, used to … built into or separate from the keyboard device.",
+        },
+      ],
+      synonyms: ["electronic keyboard"],
+    },
+    {
+      definitions: [
+        {
+          definition: "To type on a computer keyboard.",
+          example: "Keyboarding is the part of this job I hate the most.",
+        },
+      ],
+    },
+  ],
+  phonetics: [
+    {
+      text: "/ˈkibɔɹd/",
+      audio:
+        "https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3",
+      sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=1755168",
+    },
+  ],
+  phonetic: "/ˈkiːbɔːd/",
+  sourceUrls: ["https://en.wiktionary.org/wiki/keyboard"],
+};
+
+export default deFaultdata;
